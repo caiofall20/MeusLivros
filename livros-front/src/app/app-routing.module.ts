41c8e515637b9c367335ab70c7492dae1dccf8c9
Carol_Patricio <ana.patricio@parnamirim.rn.gov.br>
2019-10-02 13:22:49 -0300

    Criei os componentes de header,home,books e loans e configurei as rotas entre eles

--- livros-front/src/app/app-routing.module.ts
+++ livros-front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoansComponent } from './loans/loans.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'emprestimo' , component: LoansComponent},
+  {path: 'meuslivros', component: BooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- livros-front/src/app/app.module.ts
+++ livros-front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoansComponent } from './loans/loans.component';
+import { BooksComponent } from './books/books.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoansComponent,
+    BooksComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
