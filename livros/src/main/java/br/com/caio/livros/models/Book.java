350def5bfcbd2b4246ed37deb95dd062c71b4a83
caio <caiomateusgames@gmail.com>
2019-11-28 11:00:08 -0300

    Adicionando os outros atributos do livro.

--- livros/src/main/java/br/com/caio/livros/models/Book.java
+++ livros/src/main/java/br/com/caio/livros/models/Book.java
@@ -1,6 +1,7 @@
 package br.com.caio.livros.models;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +16,51 @@ public class Book implements Serializable {
 private Long id;
 private String title;
 private String gender;
+private String isbn;
+private String type;
+private String summary;
+private Date publication_date;
+private String number_pages;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public Date getPublication_date() {
+		return publication_date;
+	}
+
+	public void setPublication_date(Date publication_date) {
+		this.publication_date = publication_date;
+	}
+
+	public String getNumber_pages() {
+		return number_pages;
+	}
+
+	public void setNumber_pages(String number_pages) {
+		this.number_pages = number_pages;
+	}
 
 	public static long getSerialVersionUID() {
 		return serialVersionUID;
@@ -65,13 +111,18 @@ private String author;
 
 
 
-public Book(Long id, String title, String gender, String publishy_company, String author) {
+public Book(Long id, String title, String gender, String publishy_company, String author, String isbn, String type, String summary, Date publication_date, String number_pages ) {
 	super();
 	this.id = id;
 	this.title = title;
 	this.gender = gender;
 	this.publishy_company = publishy_company;
 	this.author = author;
+	this.isbn = isbn;
+	this.type = type;
+	this.summary = summary;
+	this.publication_date = publication_date;
+	this.number_pages = number_pages;
 	
 }
 
